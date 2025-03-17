c124402fedc50657d7b1c95198c94b646175f4b4
jayasupriyasomasani <jayasupriya.somasani@narayanagroup.com>
2025-03-17 12:27:31 +0530

    ref: Added ui changes in the login screen

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -3,4 +3,12 @@ class AppConstants {
       "Connect easily with your family and friends over countries";
   static const termsAndPrivacy = "Terms & Privacy Policy";
   static const startMessaging = "Start Messaging";
+  static const login = "Login";
+  static const welcomeBack = "Welcome Back";
+  static const signInToContinue = "Sign in to continue";
+  static const email = "Email";
+  static const password = "Password";
+  static const forgotPassword = "Forgot Password?";
+  static const signUp ="Sign Up";
+  static const donTHaveAccount = "Don't have account? ";
 }
--- lib/presentation/screens/auth/login/login_screen.dart
+++ lib/presentation/screens/auth/login/login_screen.dart
@@ -1,6 +1,12 @@
+import 'package:chat_app/config/theme/app_theme.dart';
 import 'package:chat_app/core/constants/app_sizes.dart';
+import 'package:chat_app/core/widgets/custom_gradient_button.dart';
 import 'package:chat_app/core/widgets/custom_text_fields.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../config/theme/colors.dart';
+import '../../../../core/constants/app_constants.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -18,39 +24,72 @@ class _LoginScreenState extends State<LoginScreen> {
     return SafeArea(
       child: Scaffold(
         /// Here form is used for validating the user credentials
-        body: Form(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: AppSizes.size30),
-            Text("Welcome Back",
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineMedium
-                    ?.copyWith(fontWeight: FontWeight.bold)),
-            SizedBox(height: 10),
-            Text(
-              "Sign in to continue",
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyLarge
-                  ?.copyWith(color: Colors.grey),
-            ),
-            SizedBox(height: 20),
-            CustomTextFields(
-              textEditingController: email,
-              hintText: 'Email',
-              prefixIcon: Icon(Icons.email),
-            ),
-            CustomTextFields(
-              textEditingController: password,
-              hintText: "Password",
-              obscureText: true,
-              suffixIcon: Icon(Icons.visibility),
-            )
-          ],
-        )),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: AppSizes.size8),
+          child: Form(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: AppSizes.size30),
+              Text(AppConstants.welcomeBack,
+                  style: Theme.of(context)
+                      .textTheme
+                      .headlineMedium
+                      ?.copyWith(fontWeight: FontWeight.bold)),
+              SizedBox(height: AppSizes.size10),
+              Text(AppConstants.signInToContinue,
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyLarge
+                      ?.copyWith(color: Colors.grey)),
+              SizedBox(height: AppSizes.size20),
+              CustomTextFields(
+                textEditingController: email,
+                hintText: AppConstants.email,
+                prefixIcon: Icon(Icons.email),
+              ),
+              SizedBox(height: AppSizes.size20),
+              CustomTextFields(
+                textEditingController: password,
+                hintText: AppConstants.password,
+                prefixIcon: Icon(Icons.lock_outline),
+                obscureText: true,
+                suffixIcon: Icon(Icons.visibility),
+              ),
+              SizedBox(height: AppSizes.size30),
+              Text(AppConstants.forgotPassword),
+              SizedBox(height: AppSizes.size10),
+              CustomGradientButton(
+                  onPressed: () {},
+                  text: AppConstants.login,
+                  textStyle: TextStyle(
+                      color: AppColors.whiteFeather,
+                      fontWeight: FontWeight.bold),
+                  gradientColors: [
+                    AppTheme.primaryColor,
+                    AppTheme.primaryColor
+                  ],
+                  borderRadius: AppSizes.size8),
+              SizedBox(height: AppSizes.size8),
+              Center(
+                child: RichText(
+                  text: TextSpan(
+                      text: AppConstants.donTHaveAccount,
+                      style: TextStyle(color: Colors.grey.shade600),
+                      children: [
+                        TextSpan(
+                            text: AppConstants.signUp,
+                            style: TextStyle(
+                                color: Theme.of(context).primaryColor,
+                                fontWeight: FontWeight.bold),
+                            recognizer: TapGestureRecognizer()) ///used to add onTop functionality
+                      ]),
+                ),
+              ),
+            ],
+          )),
+        ),
       ),
     );
   }
