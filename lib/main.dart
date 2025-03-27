f745fc09847b30e4a551e649618467ba61a82b7f
jayasupriyasomasani <jayasupriya.somasani@narayanagroup.com>
2025-03-27 11:18:56 +0530

    ref: Added firebase dependencies in gradle

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:chat_app/config/theme/app_theme.dart';
 import 'package:chat_app/presentation/screens/auth/login/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  // Ensure Flutter engine is initialized before using any plugins
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
